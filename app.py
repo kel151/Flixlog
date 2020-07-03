e250d91708b510317f483bf11a6a4d9fa71a8a1d
kel151 <kelvin.dafiemubo3@mail.dcu.ie>
2020-07-03 19:35:49 +0000

    resolving deployment issue

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+if path.exists("env.py"):
+import env
 
 app.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME')
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
@@ -11,11 +13,11 @@ app = Flask(__name__)
 mongo = PyMongo(app)
 
 @app.route("/")
-@app.route("/get_entries")
 def get_entries():
     return render_template("entries.html", entries=mongo.db.entries.find())
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '127.0.0.1'),
-            port=int(os.environ.get('PORT', '8080')),
-            debug=True)
+    app.secret_key = 'mysecret'
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True
