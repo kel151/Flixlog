d7772ca8d919102ad6b7a0cea6ce66dc24e8f2cb
kel151 <kelvin.dafiemubo3@mail.dcu.ie>
2020-07-03 19:49:04 +0000

    resolved issue runnng/previewing code and updated gitignore

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
+from os import path
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if path.exists("env.py"):
-import env
+    import env
+
+app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME')
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
-
-app = Flask(__name__)
 
 mongo = PyMongo(app)
 
@@ -17,7 +18,6 @@ def get_entries():
     return render_template("entries.html", entries=mongo.db.entries.find())
 
 if __name__ == '__main__':
-    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True
+            debug=True)
