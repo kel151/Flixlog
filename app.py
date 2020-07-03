5382af71e0272007da1ebd6ce5a945a3b3d707b7
Kelvin <kelvin.dafiemubo3@mail.dcu.ie>
2020-07-03 01:25:46 +0100

    connected flask to mongodb database

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "mav"
+app.config["MONGO_URI"] = "mongodb+srv://floppypancake:ms3ser342@myfirstcluster-frg8m.mongodb.net/mav?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World...again"
+@app.route("/get_entries")
+def get_entries():
+    return render_template("entries.html", entries=mongo.db.entries.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '127.0.0.1'),
